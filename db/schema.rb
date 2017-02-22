0f83be511903a35f43ccc880a95512c36bd45a70
Milan Thakkar <milanthakkar1@gmail.com>
2017-02-22 21:45:00 +0000

    Finallll

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222200609) do
+ActiveRecord::Schema.define(version: 20170222195034) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
